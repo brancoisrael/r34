3dc3a24f3e18c4175875e62bc5bb0489142ddea8
israel <brancoisrael@gmail.com>
2018-08-25 19:16:05 -0300

    V1

---
+++ resistencia-ws/src/main/java/br/com/r34/conf/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package br.com.r34.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	static final String DESCRIPTION = "Resistencia WS - arquitetura de microservicos";
+
+	@Bean
+	public Docket apiLOC() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.r34.services"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiLOCInfo());
+	}
+
+	private ApiInfo apiLOCInfo() {
+		return new ApiInfoBuilder().title("Visão Geral").description(DESCRIPTION).build();
+	}
+
+}
---
+++ resistencia-ws/src/main/java/br/com/r34/services/Login.java
@@ -0,0 +1,26 @@
+package br.com.r34.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+@Api(tags = "login", value = "login")
+@RestController
+public class Login {
+
+	@ApiOperation(value = "login", notes = "Login.", protocols = "Accept=application/json", response = String.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = String.class) })	
+	@RequestMapping(value="/teste",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public String teste() {
+		return "teste";
+	}
+}
