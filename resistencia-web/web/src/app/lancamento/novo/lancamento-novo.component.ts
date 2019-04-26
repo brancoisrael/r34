8f41e105246f9bb6cb2e803db3735def068d3a62
israelb.cast <israelb.cast@anatel.gov.br>
2019-04-26 17:58:58 -0300

    organização do front

---
+++ resistencia-web/web/src/app/lancamento/novo/lancamento-novo.component.ts
@@ -0,0 +1,217 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
+import {Router, ActivatedRoute} from '@angular/router'
+
+import { MessageService } from 'primeng/components/common/messageservice';
+import {ProdutoService} from '../../produto/produto.service';
+import {LancamentoService} from '../../lancamento/lancamento.service';
+import { MembroService } from '../../membros/membros.service';
+import {TIPO_LANCAMENTO,ORIGEM_DEBITO_LANCAMENTO,ORIGEM_CREDITO_LANCAMENTO,STATUS_LANCAMENTO, LancamentoModel} from '../../lancamento/modelo/lancamento.model'
+
+import {TipoProdutoModel} from '../../produto/modelo/tipo-produto.model';
+import {ProdutoModel} from '../../produto/modelo/produto.model';
+import {ProdutoVendaModel} from '../../produto/modelo/produto-venda.model';
+
+import { SelectOptions } from '../../components/select/select-options';
+import { MembroModel } from '../../membros/modelo/membro.model';
+import { ProdutoVendaDTO } from '../../produto/modelo/produto-venda.dto';
+import { LancamentoDTO } from '../modelo/lancamento.dto';
+
+
+
+@Component({
+  selector: 'app-lancamento-novo',
+  templateUrl: './lancamento-novo.component.html'
+})
+export class LancamentoNovoComponent implements OnInit {
+
+  produtoVenda:ProdutoVendaModel
+  tiposProdutos:SelectOptions[]
+  produtos:SelectOptions[]
+  membros:SelectOptions[]
+  tiposLancamentos=TIPO_LANCAMENTO
+  lancamentos:LancamentoModel[]
+  origemLancamentos:any
+  produtoDisable:boolean
+  formHidden:boolean
+  header:string='Cadastrar novo lançamento'
+  orderForm: FormGroup
+  numeroPattern =/^[0-9]*$/
+  moedaPattern =/^[0-9,.]*$/
+  dataHoje:Date=new Date()
+  membroSelecionado:MembroModel
+  membro:string
+  msg:string
+ 
+  constructor(
+    private lancamentoService:LancamentoService,
+    private produtoService:ProdutoService,
+    private membroService:MembroService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService) {
+      this.produtoDisable=true;
+      this.formHidden=true
+  }
+
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      dataLancamento:this.formBuilder.control('',Validators.required),
+      membro:this.formBuilder.control('',[Validators.required]),
+      tipoProduto:this.formBuilder.control(''),
+      observacao:this.formBuilder.control(''),
+      produto:this.formBuilder.control(''),
+      tipoLancamento:this.formBuilder.control(''),
+      origemLancamento:this.formBuilder.control('',[Validators.required]),
+      quantidade:this.formBuilder.control('',[Validators.required,Validators.pattern(this.numeroPattern)]),
+      valorLancamento:this.formBuilder.control('',[Validators.required,Validators.pattern(this.moedaPattern)])
+    })
+    this.listarMembros();
+  }
+
+  listarMembros(){
+    this.membroService.buscarMembroStatus(true)
+      .subscribe((response:MembroModel[])=>{
+        var mb:MembroModel[]=response
+        this.membros = [{label:'Selecione',value:null}];
+        
+        for(var i=0;i<mb.length;i++){          
+          this.membros.push(new SelectOptions(mb[i].nome,mb[i]));
+        }
+      })
+  }
+
+  hiddenForms(event){
+    this.formHidden=this.orderForm.get('dataLancamento')?false:true;
+  }
+
+  changeTipoProduto(event){
+    this.produtoVenda=null;
+    this.orderForm.controls['produto'].setValue(null);
+    this.produtos=null;
+
+    this.produtoService.listarProdutosVenda(this.orderForm.controls['tipoProduto'].value)
+      .subscribe((response:ProdutoModel[])=>{
+        var pr:ProdutoModel[] = response;
+        this.produtos = [{label:'Selecione',value:null}];
+        for(var i=0;i<pr.length;i++){
+          this.produtos.push(new SelectOptions(pr[i].descricao,pr[i].id))
+        }
+      })
+  }
+
+  changeTipoLancamento(event){
+    this.produtoVenda=null;
+    this.orderForm.controls['origemLancamento'].setValue(null);
+    this.orderForm.controls['tipoProduto'].setValue(null);
+    this.orderForm.controls['produto'].setValue(null);
+    this.orderForm.controls['valorLancamento'].setValue(0);
+    this.produtoDisable=true;
+    this.origemLancamentos = null;
+    
+    if(this.orderForm.controls['tipoLancamento'].value==='CREDITO')
+      this.origemLancamentos = ORIGEM_CREDITO_LANCAMENTO;
+    else if(this.orderForm.controls['tipoLancamento'].value==='DEBITO')
+      this.origemLancamentos = ORIGEM_DEBITO_LANCAMENTO;
+  }
+
+  changeOrigemLancamento(event){
+    this.produtoVenda=null;
+    this.produtoDisable=true;
+    this.orderForm.controls['quantidade'].setValue(1);
+    this.orderForm.controls['valorLancamento'].setValue(0);
+    this.orderForm.controls['tipoProduto'].setValue(null);
+    this.orderForm.controls['produto'].setValue(null);
+   
+    if(this.orderForm.controls['origemLancamento'].value==='BAR'){
+      this.listarTipoProduto();
+      this.produtoDisable=false;
+    }      
+  }
+
+  listarTipoProduto(){
+    this.produtoService.listarTipoProduto()
+      .subscribe((response:TipoProdutoModel[])=>{
+        var tp:TipoProdutoModel[]=response
+        this.tiposProdutos = [{label:'Selecione',value:null}];
+        
+        for(var i=0;i<tp.length;i++){          
+          this.tiposProdutos.push(new SelectOptions(tp[i].tipoProduto,tp[i].id));
+        }
+      })
+  }
+
+  calcPrecoVendaProduto(event){
+
+    var dto:ProdutoVendaDTO=new ProdutoVendaDTO(this.orderForm.controls['produto'].value,this.orderForm.controls['dataLancamento'].value);
+   
+    this.produtoService.selectProdutoVendaByData(dto)
+      .subscribe((response:ProdutoVendaModel)=>{
+        this.produtoVenda=response;
+
+        if(this.orderForm.controls['quantidade'].value===undefined)
+          this.orderForm.controls['quantidade'].setValue(1);
+        
+        this.orderForm.controls['valorLancamento'].setValue(response.preco);
+      })
+  }
+
+  salvarLancamento(lancamento:LancamentoModel){
+    lancamento.produtoVenda=this.produtoVenda;
+
+    this.lancamentoService.salvarLancamento(lancamento)
+      .subscribe((response:LancamentoDTO)=>{
+        this.messageService.clear();
+        this.messageService.add({severity:response.sucesso?'success':'error', summary:'Mensagem: ', detail:response.message}); 
+
+        if(response.sucesso){
+          for(var i=0;i<response.lancamentos.length;i++){            
+            this.lancamentos.push(response.lancamentos[i])
+          }
+          this.membroSelecionado.saldoMembro = response.saldoMembro;
+        }
+      })           
+  }
+
+  excluirLancamento(lancamento:LancamentoModel){
+  
+    this.lancamentoService.excluirLancamento(lancamento)
+      .subscribe((response:LancamentoDTO)=>{
+        this.messageService.clear();
+        this.messageService.add({severity:response.sucesso?'success':'error', summary:'Mensagem: ', detail:response.message}); 
+
+        if(response.sucesso){   
+            const index = this.lancamentos.indexOf(lancamento,0);
+            if(index!==-1)
+              this.lancamentos.splice(index, 1);           
+          this.membroSelecionado.saldoMembro = response.saldoMembro;
+        }
+      })           
+  }
+
+  listarLancamento(event){
+    this.membroSelecionado = this.orderForm.controls['membro'].value;
+    this.lancamentos = null;
+    if(this.membroSelecionado!==null && this.membroSelecionado!==undefined){
+      this.lancamentoService.listarLancamento(this.membroSelecionado.id)
+        .subscribe((response:LancamentoModel[])=>{
+          this.lancamentos=response;
+        })           
+    }
+  }
+
+  findTipoLancamento(key:string):string{
+    return TIPO_LANCAMENTO.find(tp=>tp.value == key).label;
+  }
+
+  findOrigemLancamento(key:string):string{
+    var ret = ORIGEM_DEBITO_LANCAMENTO.find(tp=>tp.value == key);
+    if(!ret){
+      ret = ORIGEM_CREDITO_LANCAMENTO.find(tp=>tp.value == key);
+    }
+
+    return ret.label;
+  }
+}
---
+++ resistencia-web/web/src/app/membros/novo/membro-novo.component.ts
@@ -0,0 +1,143 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {Router, ActivatedRoute} from '@angular/router'
+
+import {MembroService} from '../membros.service';
+import {MembroModel, PATENTES, CARGOS, SITUACOES} from '../modelo/membro.model';
+import { MessageService } from 'primeng/components/common/messageservice';
+import { MembroDTO } from '../modelo/membro.dto';
+
+
+
+@Component({
+  selector: 'app-membro-novo',
+  templateUrl: './membro-novo.component.html'
+})
+export class MembroNovoComponent implements OnInit {
+
+  msg:string
+  header:string='Cadastrar novo membro'
+  membro:MembroModel
+  orderForm: FormGroup
+  visibleBtNovo:boolean=true
+  dataSaidaDisable:boolean=true
+  patenteDisable:boolean=true
+  minDateNascimento:Date=new Date()
+  minDateEntrada:Date
+  minDateSaida:Date
+  patentes=PATENTES
+  situacoes=SITUACOES
+  cargos=CARGOS
+  mailPattern =/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numeroPattern =/^[0-9]*$/
+  alfaPattern=/[A-Za-z]/
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private membroService: MembroService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService) {
+      this.minDateNascimento.setFullYear(1900);
+     }
+
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      nome:this.formBuilder.control('',[Validators.required,Validators.max(50), Validators.pattern(this.alfaPattern)]),
+      apelido:this.formBuilder.control('') ,
+      email:this.formBuilder.control('',[Validators.required,Validators.pattern(this.mailPattern)]) ,
+      senha:this.formBuilder.control('',Validators.required) ,
+      dataNascimento:this.formBuilder.control('') ,
+      dataEntrada:this.formBuilder.control('',Validators.required) , 
+      dataSaida:this.formBuilder.control('') ,
+      patente:this.formBuilder.control('',[Validators.required]),
+      cargo:this.formBuilder.control('') ,
+      situacaoMembro:this.formBuilder.control('',Validators.required),
+      id:this.formBuilder.control('') ,
+    })
+
+    if(this.route.snapshot.params.id)
+    this.prepararEditar(this.route.snapshot.params.id) 
+  }
+
+  changePatente(event){
+    this.patenteDisable=true;
+    this.orderForm.get('cargo').setValue(null);
+
+    if(this.orderForm.get('patente').value ==='ESCUDADO'){
+      this.patenteDisable=false;
+    }
+  }
+
+  changeSituacao(event){
+    this.dataSaidaDisable=true;
+    if(this.orderForm.get('situacaoMembro').value ==='DESLIGADO'){
+      this.dataSaidaDisable=false;
+    }
+  }
+
+  setMinDateEntrada(event){
+    this.minDateEntrada = this.orderForm.get('dataNascimento').value;
+    this.orderForm.get('dataEntrada').setValue(null);
+  }
+
+  setMinDateSaida(event){
+    this.minDateSaida = this.orderForm.get('dataEntrada').value;
+    this.orderForm.get('dataSaida').setValue(null);
+  }
+
+  prepararEditar(id:number){
+    this.header = 'Atualizar membro';
+    this.visibleBtNovo=false;
+    this.membroService.buscarMembroID(id)
+    .subscribe((membro:MembroModel)=>{
+     this.orderForm.controls['dataNascimento'].setValue(new Date(membro.dataNascimento));
+     this.orderForm.controls['dataEntrada'].setValue(new Date(membro.dataEntrada));
+     this.orderForm.controls['dataSaida'].setValue(membro.situacaoMembro==='DESATIVADO'? new Date(membro.dataSaida):null);
+     this.orderForm.controls['situacaoMembro'].setValue(membro.situacaoMembro);
+     this.orderForm.controls['patente'].setValue(membro.patente);
+     this.orderForm.controls['cargo'].setValue(membro.cargo);
+     this.orderForm.controls['nome'].setValue(membro.nome);
+     this.orderForm.controls['apelido'].setValue(membro.apelido);
+     this.orderForm.controls['senha'].setValue(membro.senha);
+     this.orderForm.controls['email'].setValue(membro.email);
+     this.orderForm.controls['id'].setValue(membro.id);   
+     
+     this.changeSituacao(null);
+     this.patenteDisable=this.orderForm.get('patente').value ==='ESCUDADO'?false:true;
+     this.minDateEntrada = this.orderForm.get('dataNascimento').value;
+     this.minDateSaida = this.orderForm.get('dataEntrada').value;
+     
+    })
+  }
+
+  salvarMembro(membro:MembroModel){
+    membro.status=membro.situacaoMembro==='DESLIGADO'?false:true;
+
+    this.membroService.salvarMembro(membro)
+      .subscribe((response:MembroDTO)=>{
+        this.router.navigate(['/membro-novo'])
+        this.messageService.clear();
+        this.messageService.add({severity:response.sucesso?'success':'error', summary:'Mensagem: ', detail:response.message}); 
+        
+        if(response.sucesso){
+          this.orderForm.reset();
+        }
+      })     
+  }
+
+  atualizarMembro(membro:MembroModel){
+    membro.status=membro.situacaoMembro==='DESLIGADO'?false:true;
+
+    this.membroService.atualizarMembro(membro)
+      .subscribe((response:MembroDTO)=>{
+        this.messageService.clear();
+        this.messageService.add({severity:response.sucesso?'success':'error', summary:'Mensagem: ', detail:response.message}); 
+        
+        if(response.sucesso){
+          this.router.navigate(['/membros'])        
+        }
+      })     
+  }
+}
