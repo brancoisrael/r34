c9d69073d71e3116da5282f9fd218741e3b3dcd4
Israel <you@example.com>
2020-02-14 00:56:51 -0300

    ajustes frontend

--- resistencia-web/web/src/app/app.api.ts
+++ resistencia-web/web/src/app/app.api.ts
@@ -1,1 +1,7 @@
-Unexpected error.  File contents could not be restored from local history during undo/redo.
+import {Headers} from '@angular/http'
+
+
+export const R34_API='http://localhost:8080/resistencia-ws';
+
+export const HEADERS = new Headers()
+.append('Content-Type','application/json');
