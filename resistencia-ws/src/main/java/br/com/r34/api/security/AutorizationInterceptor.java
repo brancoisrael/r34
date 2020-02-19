da7fa7f4f497d75a229f2cacbea576d4b6d7775b
Israel <you@example.com>
2020-02-19 01:48:15 -0300

    ajuste interceptor cors

--- resistencia-ws/src/main/java/br/com/r34/api/security/AutorizationInterceptor.java
+++ resistencia-ws/src/main/java/br/com/r34/api/security/AutorizationInterceptor.java
@@ -9,9 +9,20 @@ import br.com.r34.service.util.JWT;
 
 public class AutorizationInterceptor extends HandlerInterceptorAdapter{
 
+	
+	
 	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+		if (request.getMethod().equals("OPTIONS")) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+            response.addHeader("Access-Control-Allow-Methods","GET, POST, PUT, OPTIONS, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,Authorization,If-Modified-Since,Cache-Control,Content-Type");
+            response.addHeader("Access-Control-Max-Age", "3600");
+            response.addHeader("charset", "utf-8");           
+        }	
+		
 	   String token = request.getHeader("authorization");
        /*System.out.println(request.getHeader("authorization"));
        System.out.println(request.getRequestURL().toString());*/
